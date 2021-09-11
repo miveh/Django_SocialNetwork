969c6b747c725c824b32ed08624406447e995136
miveh <imivechi@gmail.com>
2021-09-11 22:17:54 +0430

    fix project

--- manage.py
+++ manage.py
@@ -5,8 +5,7 @@ import sys
 
 
 def main():
-    """Run administrative tasks."""
-    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network_book.settings')
+    os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'bffbook.settings')
     try:
         from django.core.management import execute_from_command_line
     except ImportError as exc:
